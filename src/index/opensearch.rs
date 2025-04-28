3bf2e1ba3172e9ee52d912ff226159260a360d2b
Michał Hudobski <michal.hudobski@scylladb.com>
2025-04-28 14:02:33 +0200

    create backend for Opensearch

--- src/index/opensearch.rs
+++ src/index/opensearch.rs
@@ -16,10 +16,17 @@ use crate::PrimaryKey;
 use crate::index::actor::Index;
 use bimap::BiMap;
 use opensearch::OpenSearch;
+use opensearch::indices::IndicesCreateParts;
+use serde_json::json;
 use std::sync::Arc;
 use std::sync::RwLock;
 use std::sync::atomic::AtomicU64;
+use tokio::sync::Semaphore;
 use tokio::sync::mpsc;
+use tracing::Instrument;
+use tracing::debug;
+use tracing::debug_span;
+use tracing::error;
 use tracing::info;
 
 pub struct OpenSearchIndexFactory {
@@ -82,21 +89,110 @@ struct Key(u64);
 
 pub fn new(
     id: IndexId,
-    _dimensions: Dimensions,
-    _connectivity: Connectivity,
-    _expansion_add: ExpansionAdd,
-    _expansion_search: ExpansionSearch,
-    _client: Arc<OpenSearch>,
+    dimensions: Dimensions,
+    connectivity: Connectivity,
+    expansion_add: ExpansionAdd,
+    expansion_search: ExpansionSearch,
+    client: Arc<OpenSearch>,
 ) -> anyhow::Result<mpsc::Sender<Index>> {
     info!("Creating new index with id: {id}");
     // TODO: The value of channel size was taken from initial benchmarks. Needs more testing
     const CHANNEL_SIZE: usize = 100000;
-    let (tx, mut _rx) = mpsc::channel(CHANNEL_SIZE);
+    let (tx, mut rx) = mpsc::channel(CHANNEL_SIZE);
+
+    tokio::spawn(
+        {
+            let id = id.clone();
+            async move {
+                let response = client
+                    .indices()
+                    .create(IndicesCreateParts::Index(&id.0))
+                    .body(json!({
+                        "settings": {
+                            "index.knn": true
+                        },
+                        "mappings": {
+                            "properties": {
+                                "vector": {
+                                    "type": "knn_vector",
+                                    "dimension": dimensions.0.get(),
+                                    "method": {
+                                        "name": "hnsw",
+                                        "parameters": {
+                                            "ef_search": if expansion_search.0 > 0 {
+                                                expansion_search.0
+                                            } else {
+                                                100
+                                            },
+                                            "ef_construction": if expansion_add.0 > 0 {
+                                                expansion_add.0
+                                            } else {
+                                                100
+                                            },
+                                            "m": if connectivity.0 > 0 {
+                                                connectivity.0
+                                            } else {
+                                                16
+                                            },
+                                        }
+                                    }
+                                },
+                            }
+                        }
+                    }))
+                    .send()
+                    .await
+                    .map_or_else(
+                        |err| Err(err),
+                        opensearch::http::response::Response::error_for_status_code,
+                    )
+                    .map_err(|err| {
+                        error!("engine::new: unable to create index with id {id}: {err}");
+                    });
+
+                if response.is_err() {
+                    return;
+                }
+
+                debug!("starting");
+
+                // bimap between PrimaryKey and Key for an usearch index
+                let keys = Arc::new(RwLock::new(BiMap::new()));
+
+                // Incremental key for a usearch index
+                let opensearch_key = Arc::new(AtomicU64::new(0));
+
+                // This semaphore decides how many tasks are queued for an usearch process. It is
+                // calculated as a number of threads multiply 2, to be sure that there is always a new
+                // task waiting in the queue.
+                let semaphore = Arc::new(Semaphore::new(rayon::current_num_threads() * 2));
+
+                let id = Arc::new(id);
+
+                while let Some(msg) = rx.recv().await {
+                    let permit = Arc::clone(&semaphore).acquire_owned().await.unwrap();
+                    tokio::spawn({
+                        let id = Arc::clone(&id);
+                        let keys = Arc::clone(&keys);
+                        let opensearch_key = Arc::clone(&opensearch_key);
+                        let client = Arc::clone(&client);
+                        async move {
+                            process(msg, dimensions, id, keys, opensearch_key, client).await;
+                            drop(permit);
+                        }
+                    });
+                }
+
+                debug!("finished");
+            }
+        }
+        .instrument(debug_span!("opensearch", "{id}")),
+    );
 
     Ok(tx)
 }
 
-async fn _process(
+async fn process(
     msg: Index,
     _dimensions: Dimensions,
     _id: Arc<IndexId>,
