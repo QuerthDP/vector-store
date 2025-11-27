afbc6571948c5bab10f1607842d11cd4906bfec1
Dawid Pawlik <dawid.pawlik@scylladb.com>
2025-11-27 13:06:13 +0100

    Select returns wrong results after node restart

--- crates/validator-vector-store/src/reconnect.rs
+++ crates/validator-vector-store/src/reconnect.rs
@@ -30,6 +30,11 @@ pub(crate) async fn new() -> TestCase {
             timeout,
             restarting_all_nodes_doesnt_break_fullscan,
         )
+        .with_test(
+            "restarting_nodes_only_driver",
+            timeout,
+            restarting_nodes_only_driver,
+        )
 }
 
 async fn reconnect_doesnt_break_fullscan(actors: TestActors) {
@@ -240,6 +245,72 @@ async fn restarting_all_nodes_doesnt_break_fullscan(actors: TestActors) {
         )
         .await
         .expect("failed to query ANN search");
+
+    session
+        .query_unpaged(format!("DROP KEYSPACE {keyspace}"), ())
+        .await
+        .expect("failed to drop a keyspace");
+
+    info!("finished");
+}
+
+async fn restarting_nodes_only_driver(actors: TestActors) {
+    info!("started");
+
+    let (session, _) = prepare_connection(&actors).await;
+
+    let keyspace = create_keyspace(&session).await;
+    let table = create_table(
+        &session,
+        "id INT PRIMARY KEY, embedding VECTOR<FLOAT, 3>",
+        None,
+    )
+    .await;
+
+    let stmt = session
+        .prepare(format!(
+            "INSERT INTO {table} (id, embedding) VALUES (?, [1.0, 2.0, 3.0])"
+        ))
+        .await
+        .expect("failed to prepare a statement");
+
+    for id in 0..1000 {
+        session
+            .execute_unpaged(&stmt, (id,))
+            .await
+            .expect("failed to insert a row");
+    }
+
+    let results = get_query_results(format!("SELECT * FROM {table}"), &session).await;
+    let rows = results
+        .rows::<(i32, Vec<f32>)>()
+        .expect("failed to get rows");
+    assert_eq!(rows.rows_remaining(), 1000);
+
+    let db_ips = get_default_db_ips(&actors);
+
+    // Restart each node one by one
+    for db_ip in db_ips {
+        info!("Restarting node {}", db_ip);
+        actors
+            .db
+            .restart(get_default_vs_url(&actors).await, db_ip)
+            .await;
+
+        let results = get_query_results(format!("SELECT * FROM {table}"), &session).await;
+        let rows = results
+            .rows::<(i32, Vec<f32>)>()
+            .expect("failed to get rows");
+        info!("rows after restart of {}: {}", db_ip, rows.rows_remaining());
+
+        // sleep(Duration::from_secs(2)).await;     // <- The sleep fixes the test
+    }
+
+    let results = get_query_results(format!("SELECT * FROM {table}"), &session).await;
+    let rows = results
+        .rows::<(i32, Vec<f32>)>()
+        .expect("failed to get rows");
+    assert_eq!(rows.rows_remaining(), 1000);
 
     session
         .query_unpaged(format!("DROP KEYSPACE {keyspace}"), ())
